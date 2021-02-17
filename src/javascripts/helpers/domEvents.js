56fbe5e5aefb190c6d9df27d0f1609851bd6620c
Sean <jb@JBs-MacBook-Pro.local>
2021-02-16 20:26:12 -0600

    modularized all of js

---
+++ src/javascripts/helpers/domEvents.js
@@ -0,0 +1,14 @@
+import { addStudentToArray, expelStudent } from './data/studentData';
+import showForm from '../components/showForm';
+
+const domEvents = () => {
+  document.querySelector('#start-sorting').addEventListener('click', showForm);
+  document
+    .querySelector('#sorting-form')
+    .addEventListener('click', addStudentToArray);
+  document
+    .querySelector('#first-years-card-display')
+    .addEventListener('click', expelStudent);
+};
+
+export default domEvents;
